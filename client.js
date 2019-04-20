41b9dbae9ea76bb9704a5c9a2cc198ae994398db
SafyreLyons <pinkdolphingaming405@gmail.com>
2019-04-20 11:01:57 -0600

    Create client.js

---
+++ client.js
@@ -0,0 +1,15 @@
+function creatorHome (){
+  window.open("https://5lt.dev", '_blank');
+}
+
+function aboutJyr (){
+  window.location.href = '#aboutJyr';
+}
+
+function home (){
+  window.location.href = 'index.html';
+}
+
+function vyx (){
+  window.location.href = '#vyx';
+}
